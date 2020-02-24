4d7b97ec6cd5c42e1afb13095d8123b9575e7e75
HumayunMulla <humayun.mulla@gmail.com>
2020-02-24 01:28:41 -0500

    Code is ready for deployment

--- mail-merge.py
+++ mail-merge.py
@@ -7,20 +7,51 @@ import datetime
 import win32com.client as win32
 import xlrd
 
+# Certain details are abstracted and defined in config.ini file
+# Config Parser
+try:
+    from configparser import ConfigParser
+except ImportError:
+    from ConfigParser import ConfigParser  # ver. < 3.0
+# instantiate
+config = ConfigParser()
+# parse existing file
+config.read('config.ini')
+mail_subject = config.get('system', 'Subject')
+BodyText1 = config.get('system', 'BodyText1')
+BodyText2 = config.get('system', 'BodyText2')
+BodyText3 = config.get('system', 'BodyText3')
+Signature = config.get('system', 'Signature')
+Designation = config.get('system', 'Designation')
+
 # print "This is working!"
 
 # Win32 API for Outlook Application
 outlook = win32.Dispatch('outlook.application')
 mail = outlook.CreateItem(0)
 
-
 # Sending email function
-def send_email():  
-    mail.To = "hmulla1@binghamton.edu"
-    mail.Subject = "Testing Mail, please ignore"
-    mail.Body = "TESTING EMAIL BODY"
-    #mail.Send() # send email
-    #print "Successfully email sent!"
+def send_email(receipient_name, to_address, body_content1, body_content2):  
+    body_detailed = body_content2.split(', ')
+    # print body_detailed
+    i = 0
+    mail_sublist = ""
+    sizeofList = len(body_detailed)
+    while i < sizeofList:
+        mail_sublist += body_content1 + "\t" + body_detailed[i] + "\n"
+        i += 1
+    # print mail_sublist
+
+    mail.To = to_address
+    mail.Subject = mail_subject
+    # print mail_subject
+    mail_body = "Hello " + receipient_name + ",\n"
+    mail_body = mail_body + "\n" + BodyText1 + "\n\n" + BodyText2 + "\n\n" + mail_sublist + "\n" + BodyText3 + "\n\nThank you.\n\n" + Signature +"\n" + Designation
+    
+    mail.Body = mail_body
+    # print mail.Body
+    mail.Send() # send email
+    print "Successfully email sent!"
 
 # calling the send email function
 # send_email()   
@@ -48,7 +79,7 @@ for row_cursor in range(1,total_rows):
     receipient_body_part1 = worksheet.cell(row_cursor,4).value
     # Receipient Body Part-2
     col_index = 5 # customized details start from this location
-    while col_index < 16:
+    while col_index < 15:
         if col_index==5:
             excel_data = worksheet.cell(row_cursor,5).value
             if excel_data !="":
@@ -69,7 +100,7 @@ for row_cursor in range(1,total_rows):
 i = 0
 sizeofList = len(receipient_email)
 while i < sizeofList:
-    print receipient_name[i] + " " + receipient_email[i] + " " + receipient_body_part1 + " " + receipient_body_part2[i]
+    send_email(receipient_name[i], receipient_email[i], receipient_body_part1, receipient_body_part2[i])
     i += 1
 
 # print receipient_body_part1
