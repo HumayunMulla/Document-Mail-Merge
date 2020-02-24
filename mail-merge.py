1db6decea84b2172de426fd7221a45e544f7cbb7
root <root@DESKTOP-862E1JD.localdomain>
2020-02-24 10:26:34 -0500

    resolved the bug with body content

--- mail-merge.py
+++ mail-merge.py
@@ -21,37 +21,41 @@ mail_subject = config.get('system', 'Subject')
 BodyText1 = config.get('system', 'BodyText1')
 BodyText2 = config.get('system', 'BodyText2')
 BodyText3 = config.get('system', 'BodyText3')
+BodyText4 = config.get('system', 'BodyText4')
 Signature = config.get('system', 'Signature')
 Designation = config.get('system', 'Designation')
 
 # print "This is working!"
 
-# Win32 API for Outlook Application
-outlook = win32.Dispatch('outlook.application')
-mail = outlook.CreateItem(0)
+
 
 # Sending email function
 def send_email(receipient_name, to_address, body_content1, body_content2):  
-    body_detailed = body_content2.split(', ')
+    # Win32 API for Outlook Application
+    outlook = win32.Dispatch('outlook.application')
+    mail = outlook.CreateItem(0)
+    body_detailed = str(body_content2).split(', ')
     # print body_detailed
     i = 0
     mail_sublist = ""
     sizeofList = len(body_detailed)
     while i < sizeofList:
-        mail_sublist += body_content1 + "\t" + body_detailed[i] + "\n"
+        mail_sublist += str(body_content1) + "\t" + body_detailed[i] + "\n"
         i += 1
     # print mail_sublist
-
+    # print receipient_name
     mail.To = to_address
     mail.Subject = mail_subject
     # print mail_subject
     mail_body = "Hello " + receipient_name + ",\n"
-    mail_body = mail_body + "\n" + BodyText1 + "\n\n" + BodyText2 + "\n\n" + mail_sublist + "\n" + BodyText3 + "\n\nThank you.\n\n" + Signature +"\n" + Designation
+    mail_body = mail_body + "\n" + BodyText1 + "\n\n" + BodyText2 + "\n\n" + mail_sublist + "\n" + BodyText3 + "\n\n"BodyText4"\n\n" + Signature +"\n" + Designation
     
     mail.Body = mail_body
     # print mail.Body
     mail.Send() # send email
     print "Successfully email sent!"
+    
+    
 
 # calling the send email function
 # send_email()   
@@ -66,7 +70,7 @@ total_rows = worksheet.nrows
 # variables used to send emails [receipient addesss, name, subject contents]
 receipient_email = []
 receipient_name = []
-receipient_body_part1 = ""
+receipient_body_part1 = []
 receipient_body_part2 = []
 
 for row_cursor in range(1,total_rows):
@@ -76,7 +80,9 @@ for row_cursor in range(1,total_rows):
     # Receipient Email Address
     receipient_email.append(worksheet.cell(row_cursor,3).value)
     # Receipient Body Part-1
-    receipient_body_part1 = worksheet.cell(row_cursor,4).value
+    # receipient_body_part1 = str(worksheet.cell(row_cursor,4).value)
+    receipient_body_part1.append(str(worksheet.cell(row_cursor,4).value))
+    
     # Receipient Body Part-2
     col_index = 5 # customized details start from this location
     while col_index < 15:
@@ -100,7 +106,8 @@ for row_cursor in range(1,total_rows):
 i = 0
 sizeofList = len(receipient_email)
 while i < sizeofList:
-    send_email(receipient_name[i], receipient_email[i], receipient_body_part1, receipient_body_part2[i])
+    send_email(receipient_name[i], receipient_email[i], receipient_body_part1[i], receipient_body_part2[i])
+
     i += 1
 
 # print receipient_body_part1
@@ -113,3 +120,4 @@ system_date = datetime.datetime.now().strftime('%d%m%Y'+"_"+'%H%M%S')
 # system_date variable is used later in the program for naming the file.
 
 # Generate log for every email sent
+
